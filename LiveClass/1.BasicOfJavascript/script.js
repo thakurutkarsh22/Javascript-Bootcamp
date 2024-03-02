d42b0cfa3b8dea3f719dfc086e1af29333521f6d
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-03-02 12:56:53 +0530

    Basics of Javascript: variable

---
+++ LiveClass/1.BasicOfJavascript/script.js
@@ -0,0 +1,108 @@
+console.log("hello!!!!");
+
+// 1;
+// 2;
+// 3;
+// 4;
+// 5;
+
+// var math = 100;
+// var physic = 60;
+
+// var sum = physic + math;
+
+// console.log(sum);
+
+// PROGRAMMING LANGUAGE IS ONLY ->  Math and Function and NOTHING ELSE
+
+// --------------------- Variable Naming ----------------
+
+// Variable can never start with A NUMBER
+
+// var 1math =  NOT POSSIBLE
+// const 10 = 100  NOT POSSIBLE
+
+// var math1 = 1; // this is okay
+
+// var ma1th = 100;
+
+// // Camel casing ->
+
+// var MATH = 100;
+// var math = 100;
+
+// var maTH = 90;
+
+// var mobileringing = "yes";
+
+// var MOBILERINGING = "no";
+
+// var mobileRinging = "yes"; // this is CAMEL CASING
+
+// -------------------  VARIABLE ------------------
+
+// let const var
+
+// let num = 1;
+// const greeting = "hello";
+// var isSunAStar = true;
+
+// let const and var is used to describe the variable name only
+// that variable can take ANY VALUE. (int, string, boolean, character, objects ....)
+
+// in Mordern JS always USER let and const
+// DO NOT USE var.
+
+// Old JS -> before ES6 (ecma script version 6 - 2015)
+// Mordern JS -> ES6 and after
+
+// Let and const was introduced in ES6. before that we only had var.
+
+//  ----- const -> cant be updated once the value is assigned
+
+// const abc; // you cant do this
+
+// const pi = 3.14159265359;
+
+// pi = 100;
+
+// console.log(pi);
+
+// ---- let -> you can update later on
+
+// let math = 20;
+
+// let math; // 1. Declaration
+// math = 20 // 2. assignment.
+
+// // recheck
+
+// math = 80;
+
+// console.log("math marks: ", math);
+
+// ------ var -> you can update later on
+
+// var a = 99  // this line can be broken into 2 parts
+
+// var a;  // 1. declaration
+// a = 99 // 2. Assignment
+// console.log("a1", a);
+
+// a = 0;
+// console.log("a2", a);
+
+//  ------------------------- Strings ------------------------
+
+let score = 320;
+
+var commentary = "Hey India score is ${score}"; // hindi
+let commentary2 = "Hey Wikcket down new score is 290"; // english
+
+// String template
+
+const commentary3 = `Sore is ${score}`; // french
+
+console.log(commentary);
+console.log(commentary2);
+console.log(commentary3);
